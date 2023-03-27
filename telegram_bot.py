71286cbc0fa4ea036fe274f3ac9080fca1372a9d
Eclipter <Eclipter9000@gmail.com>
2023-03-27 22:17:19 +0300

    Refactored telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,32 +1,31 @@
 import openai
-from aiogram import Bot, Dispatcher, executor, types
 import logging
 
-from settings import *
+from aiogram import Bot, Dispatcher, executor, types
+from settings import TELEGRAM_BOT_API_KEY, OPENAI_API_KEY
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TELEGRAM_BOT_API_KEY)
-chatgpt = OPENAI_API_KEY
+openai.api_key = OPENAI_API_KEY
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['chat'])
-async def chat_cmd(message: types.message):
-    arg = message.get_args()
-    openai.api_key = chatgpt
+async def chat_cmd_handler(message: types.Message):
+    """Responds to a chat command by generating a response with OpenAI's GPT-3 model."""
+    prompt_text = message.get_args()
     model_engine = "text-davinci-003"
-    prompt = f'{arg}'
     completion = openai.Completion.create(
         engine=model_engine,
-        prompt=prompt,
+        prompt=prompt_text,
         max_tokens=1024,
-        n=1,
         stop=None,
         temperature=0
     )
     response = completion.choices[0].text
-    await message.reply(f'{response}')
+    await message.reply(response)
+
+dp.register_message_handler(chat_cmd_handler, commands=['chat'])
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
