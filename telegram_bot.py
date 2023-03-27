d92c01486e3225fc32ac0865cceefadd6523713e
Eclipter <Eclipter9000@gmail.com>
2023-03-27 18:27:03 +0300

    First commit

---
+++ telegram_bot.py
@@ -0,0 +1,16 @@
+import telebot
+
+from settings import *
+
+bot = telebot.TeleBot(TELEGRAM_BOT_API_KEY)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id, '<b>Доброго времени суток!</b>', parse_mode='HTML')
+    sticker = open('sticker.webp')
+    bot.send_sticker(message.chat.id, sticker)
+
+
+bot.infinity_polling()
