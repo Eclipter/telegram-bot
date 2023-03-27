541a17600cb0b6fb30bad472cdbb2a9738f38dc1
Eclipter <Eclipter9000@gmail.com>
2023-03-27 21:33:00 +0300

    Refactored names

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 TELEGRAM_BOT_API_KEY = os.environ.get("TELEGRAM_BOT_API_KEY")
-OPEN_AI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
